8aea6791bc5fcea63f81adcb977516f8311bc87b
MiSinG <1608839567@qq.com>
2024-02-29 16:58:48 +0800

    feat:完成获取粉丝数的rpc接口

---
+++ common/common-api/interaction-api/src/main/java/com/ysm/interaction/api/FollowServiceIRPC.java
@@ -0,0 +1,12 @@
+package com.ysm.interaction.api;
+
+/**
+ * @Description: TODO
+ * @Author MiSinG
+ * @Date 2024/2/29
+ * @Version V1.0
+ **/
+public interface FollowServiceIRPC {
+
+    Long getFollowerCount(Long userId);
+}
